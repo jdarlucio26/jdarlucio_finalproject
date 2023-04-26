b84da35e582bb6223b835f5e2348d0ce6e8b8957
jdarlucio26 <j.darlucio23@gmail.com>
2023-04-26 13:58:53 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# File created by Joshua Darlucio
+'''
+GOAL: 
+Create a program that allows you to choose your favorite Disneyland ride and build a day at Disneyland
+based on preexsisting data off a website. It will tell you a full outline of your day. 
+
+'''
+'''
+Sources:
+'''
+import webbrowser
+webbrowser.open('https://www.thrill-data.com/waits/park/dlr/disneyland/')
