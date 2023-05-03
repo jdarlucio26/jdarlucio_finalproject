35ebfce0f03cb9dc5282d1a8d159e86ca0233242
jdarlucio26 <j.darlucio23@gmail.com>
2023-05-03 10:58:02 -0700

    Added goals and sources. Start code

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 # File created by Joshua Darlucio
 '''
-GOAL: 
-Create a program that allows you to choose your favorite Disneyland ride and build a day at Disneyland
-based on preexsisting data off a website. It will tell you a full outline of your day. 
+Sources: 
+'''
+'''
+Goals:
+1) Pull ride names out of website
+2) Ask user their favorite type of ride
+3) Use favorite ride to build a list of what rides to ride, based on wait times
 
 '''
-'''
-Sources:
-'''
-import webbrowser
-webbrowser.open('https://www.thrill-data.com/waits/park/dlr/disneyland/')
+import requests
+from bs4 import BeautifulSoup
+
+# URL for Disneyland wait times
+url = 'https://www.thrill-data.com/waits/park/dlr/disneyland/'
+
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+# Find all the ride names and wait times
+rides = soup.find_all('div', class_='plot-container plotly')
+ride_data = {}
+for ride in rides:
+    name = ride.find('div', class_='col-sm-8 ride-name').text.strip()
+    wait_time = ride.find('div', class_='col-sm-4 ride-wait-time').text.strip()
+    ride_data[name] = int(wait_time)
+
