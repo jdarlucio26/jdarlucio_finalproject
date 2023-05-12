ce5bf95b9dc8c3becac4f296b11566137d04bb34
jdarlucio26 <j.darlucio23@gmail.com>
2023-05-12 10:51:12 -0700

    Added comments. Need to work on moving data

--- main.py
+++ main.py
@@ -1,22 +1,47 @@
+# Created by: Joshua Darlucio
+'''
+Sources:
+https://medium.com/analytics-vidhya/how-to-scrape-a-table-from-website-using-python-ce90d0cfb607
+https://automatetheboringstuff.com/2e/chapter12/
+https://www.geeksforgeeks.org/scrape-tables-from-any-website-using-python/
+https://realpython.com/python-web-scraping-practical-introduction/
+
+GOALS:
+1) Screen Scrape Data from website to print in terminal ✔️
+2) Get screen scraped data into a data table
+3) Have terminal ask user what their favorite ride is
+4) Have code use the favorite ride and bring up the current wait time
+
+Reach Goals:
+- Have the data recommend order of rides for the day. 
+'''
+# Libraries
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
+# Show URL and request python to go to the URL
 url = "https://www.laughingplace.com/w/p/disneyland-current-wait-times/"
 response = requests.get(url)
 
+# Allow python to screen scrape fromt the website. 
 soup = BeautifulSoup(response.content, "html.parser")
 
+# Find the things I am screen scraing. Telling it to pull data from table that follows rule of <tr>
 ride_table = soup.find("table", {"class": "lp_attraction"})
 ride_rows = ride_table.find_all("tr")[1:]
 
-ride_names = []
-wait_times = []
+# Creating a list for ride names and wait times
+ride_names = [ride_table]
+wait_times = [ride_rows]
 
+# Defining what each column will contain. 
 for row in ride_rows:
-    columns = row.find_all("t")
-    ride_names.append(columns[0].text.strip())
+    columns = row.find_all("tr")
+    ride_names.append(columns[1].text.strip())
     wait_times.append(columns[1].text.strip())
 
+# Convert data into a data frame on the screen.
 df = pd.DataFrame({'Ride Name': ride_names, 'Wait Time': wait_times})
-df.to_excel('Disneyland Wait Times.xlsx', index=False)
+
+
--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
 url = "https://www.laughingplace.com/w/p/disneyland-current-wait-times/"
 response = requests.get(url)
@@ -14,15 +13,4 @@ for row in ride_rows:
     columns = row.find_all("td")
     ride_name = columns[0].text.strip()
     wait_time = columns[1].text.strip()
-
-df = pd.DataFrame({'Ride Name': ride_name, 'Wait Time': wait_time})
-df.to_excel('Disneyland Wait Times.xlsx', index=False)
-
-#     print(f"{ride_name}: {wait_time}")
-
-
-# data = [ride_name, wait_time]
-
-# df = pd.DataFrame(data, columns=['Ride Name', 'Current Wait Time'])
-
-df
+    print(f"{ride_name}: {wait_time}")
